db0802dcb0f0f047c6ab2bcd0a7a0273db1e8f2b
Thomas Lepage <thomas.lepage@hotmail.ca>
2017-10-07 16:56:03 -0400

    Initial commit

---
+++ js/musicPlayer.js
@@ -0,0 +1,3 @@
+document.getElementById("backButton").addEventListener('click', function(){
+   window.location.replace('index.html');
+});
---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+document.getElementById("music").addEventListener('click', function(){
+   window.location.replace('music.html');
+});
+
+document.getElementById("phone").addEventListener('click', function(){
+   window.location.replace('phone.html');
+});
+
